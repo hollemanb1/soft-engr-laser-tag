055e6ddc99b3a8c2021402c35c9489d3b729320a
Mark-wl-xyz <96085770+Mark-wl-xyz@users.noreply.github.com>
2025-09-29 13:18:40 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Updating package lists..."
+sudo apt update
+
+# Install a specific package (e.g., htop)
+echo "Installing python packages"
+sudo apt install python python3 python3-pyqt5 python3-pyqt5.qtquick python3-pyqt5.qtsvg python3psycopg2
+
+echo "Packages installed. Opening application..."
+python3 main.py
