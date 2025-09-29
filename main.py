dfd614cff25bc3365ff687fb87c2f6f63e8f127a
warren <wpersonal04@gmail.com>
2025-09-28 21:35:43 -0500

    bug fixes pls work

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ Main entry point for the Photon Game.
 """
 
 import sys, time
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QTimer
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QTimer
 
 from qt_ui import ScoreboardWindow, Start_App   # <-- your old qt_header.py (rename to qt_ui.py)
 from engine import GameEngine                   # <-- new consolidated game logic
@@ -32,7 +32,7 @@ def main():
 
 
     # --- Run app event loop ---
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
