240637dcb7af581df55cda2e2cf40bf70818e3d0
Mark Livingston <96085770+lowlevel-vampyre@users.noreply.github.com>
2025-10-03 14:51:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt update
 
 # Install a specific package (e.g., htop)
 echo "Installing python packages"
-sudo apt install python python3 python3-pyqt5 python3-pyqt5.qtquick python3-pyqt5.qtsvg python3psycopg2
+sudo apt install python python3 python3-pyqt5 python3-pyqt5.qtquick python3-pyqt5.qtsvg python3-psycopg2
 
 echo "Packages installed. Opening application..."
 python3 main.py
