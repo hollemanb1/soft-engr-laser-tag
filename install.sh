49ff7f89d06b4ef4907e282b3a91cfce45b385f1
Mark Livingston <96085770+lowlevel-vampyre@users.noreply.github.com>
2025-11-06 18:44:29 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt update
 
 # Install a specific package (e.g., htop)
 echo "Installing python packages"
-sudo apt install python python3 python3-pyqt5 python3-pyqt5.qtquick python3-pyqt5.qtsvg python3-psycopg2 \
+sudo apt install python python3 python3-pyqt5 python3-pyqt5.qtquick python3-pygame python3-pyqt5.qtsvg python3-psycopg2 \
 postgresql python3-pil
 
 echo "Starting PostgreSQL..."
